b1b530995cc1277ea431bc2d21e7c3aa9c6557a4
unknown <traianmihali696@gmail.com>
2019-07-21 00:32:49 +0300

    modify get_payments to accept default parameters

--- stored procedures/get_payments.sql
+++ stored procedures/get_payments.sql
@@ -1,14 +1,32 @@
--- generated from mysql workbench
-USE `sql_invoicing`;
-DROP procedure IF EXISTS `get_payments`;
+-- Write a stored procedured called get_payments with two default parameters
+-- client_id INT
+-- payment_method_id TINYINT
+
+USE sql_invoicing;
+DROP PROCEDURE IF EXISTS get_payments;
 
 DELIMITER $$
-USE `sql_invoicing`$$
-CREATE PROCEDURE `get_payments` ()
+CREATE PROCEDURE get_payments(
+	client_id INT, 
+	payment_method_id TINYINT
+)
 BEGIN
-	SELECT * FROM payments;
+	SELECT 
+		p.payment_id, 
+		p.client_id, 
+		pm.payment_method_id, 
+        pm.name
+	FROM payments p
+	JOIN payment_methods pm
+		ON p.payment_method = pm.payment_method_id
+	WHERE p.client_id = IFNULL(client_id, p.client_id) 
+    AND pm.payment_method_id = IFNULL(payment_method_id, pm.payment_method_id);
 END$$
 
 DELIMITER ;
-    
-CALL get_payments();
+
+
+CALL get_payments(NULL, NUll);
+CALL get_payments(5, NUll);
+CALL get_payments(NULL, 1);
+CALL get_payments(5, 2);

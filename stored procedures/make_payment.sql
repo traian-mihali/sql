a5f13fc3b56bff5efd2c8d13e7e4db0cc307bfd7
unknown <traianmihali696@gmail.com>
2019-07-21 04:32:10 +0300

    add make_payment stored procedure with parameter validation

---
+++ stored procedures/make_payment.sql
@@ -0,0 +1,24 @@
+USE sql_invoicing;
+DROP procedure IF EXISTS make_payment;
+
+DELIMITER $$
+CREATE PROCEDURE make_payment (
+	invoice_id INT,
+	payment_amount DECIMAL(9, 2),
+	payment_date DATE
+)
+BEGIN
+	IF payment_amount <= 0 THEN
+		SIGNAL SQLSTATE "22003"
+			SET MESSAGE_TEXT='INVALID payment amount';
+	END IF;
+    
+	UPDATE invoices i
+    SET i.payment_total = payment_amount,
+		i.payment_date = payment_date
+	WHERE i.invoice_id = invoice_id;
+END$$
+
+DELIMITER ;
+
+CALL make_payment(2, 99, '2019-01-01');

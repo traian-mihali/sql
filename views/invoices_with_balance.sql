e6168936e0ffa429ee4c18b506591d0255b68755
unknown <traianmihali696@gmail.com>
2019-07-21 03:04:42 +0300

    add WITH CHECK OPTION to prevent update or delete to exclude rows from view

--- views/invoices_with_balance.sql
+++ views/invoices_with_balance.sql
@@ -6,7 +6,11 @@ SELECT
     c.name,
     invoice_total,
     payment_total,
-    invoice_total - payment_total AS balance
+    invoice_total - payment_total AS balance,
+    invoice_date,
+    due_date,
+    payment_date
 FROM invoices
-    JOIN clients c    
-	USING(client_id);
+JOIN clients c USING(client_id)
+WHERE (invoice_total - payment_total) > 0
+WITH CHECK OPTION;

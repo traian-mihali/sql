de1908a7b64d28c6c5d08be424667c989b41dc4b
unknown <traianmihali696@gmail.com>
2019-07-21 04:47:59 +0300

    add a stored procedure with output parameters

---
+++ stored procedures/get_unpaid_invoices_for_client.sql
@@ -0,0 +1,24 @@
+-- Output Parameters
+USE sql_invoicing;
+DROP PROCEDURE IF EXISTS get_unpaid_invoices_for_client;
+
+DELIMITER $$
+CREATE PROCEDURE get_unpaid_invoices_for_client
+(
+    client_id INT,
+    OUT invoices_count INT,
+    OUT invoices_total DECIMAL(9, 2)
+)
+BEGIN
+	SELECT COUNT(*), SUM(invoice_total)
+    INTO invoices_count, invoices_total
+    FROM invoices i
+    WHERE i.client_id = client_id AND payment_total = 0;
+END$$
+
+DELIMITER ;
+
+SET @invoices_count = 0;
+SET @invoices_total = 0;
+CALL sql_invoicing.get_unpaid_invoices_for_client(3, @invoices_count, @invoices_total);
+SELECT @invoices_count, @invoices_total;
